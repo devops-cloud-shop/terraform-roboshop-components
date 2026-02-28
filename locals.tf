9b40f059cffcfd3e607d9e60dca23502b083ba91
Prav-Akula <pravs.akula@outlook.com>
2026-02-27 19:14:46 -0500

    roboshop-infra

--- locals.tf
+++ locals.tf
@@ -9,7 +9,7 @@ locals {
     private_subnet_ids  = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
     backend_alb_listener_arn    = data.aws_ssm_parameter.backend_alb_listener_arn.value
     frontend_alb_listener_arn   = data.aws_ssm_parameter.frontend_alb_listener_arn.value
-    listener_arn                = "${var.component}" == "frontend" ? "local.frontend_alb_listerner_arn" : "local.backend_alb_listerner_arn"
+    listener_arn                = "${var.component}" == "frontend" ? "local.frontend_alb_listener_arn" : "local.backend_alb_listener_arn"
     host_context                = "${var.component}" == "frontend" ? ["${var.project_name}-${var.environment}-${var.domain_name}"] : ["${var.component}.backend_alb-${var.environment}-${var.domain_name}"]
     common_tags = {
         Project = var.project_name
